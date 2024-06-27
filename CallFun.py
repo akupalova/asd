325799dc65ea79c2d5bb3de4e2b6917f4d6fae70
PC <akupalova@yandex.ru>
2024-06-27 20:11:20 +0300

    initial

---
+++ CallFun.py
@@ -0,0 +1,21 @@
+def send_email(message, recipient,*, sender="university.help@gmail.com"):
+        if "@" in recipient and "@" in sender:
+            if (".com" in recipient or ".ru" in recipient or ".net" in recipient) and (".com" in sender or ".ru" in sender or ".net" in sender):
+                if recipient != sender:
+                    if sender == "university.help@gmail.com":
+                        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+                    else:
+                        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ", sender, "на адрес", recipient)
+                else:
+                    print("Нельзя отправить письмо самому себе!")
+
+            else:
+                print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+        else:
+            print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
